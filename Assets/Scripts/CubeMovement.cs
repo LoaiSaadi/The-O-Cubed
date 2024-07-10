fb15f187f79cfe0da3d6159421b731374736b0b7
Loai Saadi <saadiloai@gmail.com>
2024-07-10 17:26:08 +0300

    The game files

---
+++ Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class cubeMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
