53a7a83ddc055cf45f07ec634a7b3c9e6dbcf1a6
Loai Saadi <saadiloai@gmail.com>
2024-07-11 19:31:06 +0300

    Removed the jump boolean condition

--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -10,13 +10,11 @@ public class CubeMovement : MonoBehaviour
     public Rigidbody playerRb;
     private float horizontalInput;
     private float forwardInput;
-    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        isGrounded = true;
     }
 
     // Update is called once per frame
@@ -31,20 +29,11 @@ public class CubeMovement : MonoBehaviour
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
         // jump the player
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             // Vector3.up means (0,1,0): means jump up 1 unit (towards y)
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
-        }
-        isGrounded = true;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
         }
     }
+ 
 }
