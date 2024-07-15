70341c538be8b7ad9f994df834deea8dc9bed83f
Loai Saadi <saadiloai@gmail.com>
2024-07-16 00:11:40 +0300

    Added octahedron

--- Assets/Scripts/ChangeCharacter.cs
+++ Assets/Scripts/ChangeCharacter.cs
@@ -1,115 +1,82 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCharacter : MonoBehaviour
+{
+
+    public GameObject cube; // Reference to the cube
+    public GameObject sphere; // Reference to the sphere
+    public float timeToChange = 5.0f; // Time in seconds to change the character
+
+    void Start()
+    {
+        // Ensure the sphere is inactive at the start
+        sphere.SetActive(false);
+
+        /// Change once after 10 seconds
+        // Start the coroutine to change the character
+        StartCoroutine(ChangeCharacterFromCubeToSphere(timeToChange));
+    }
+
+    IEnumerator ChangeCharacterFromCubeToSphere(float time)
+    {
+        // Wait for the specified time
+        yield return new WaitForSeconds(time);
+
+        // Deactivate the cube and activate the sphere
+        cube.SetActive(false);
+        sphere.SetActive(true);
+
+        // Optional: transfer the position and rotation of the cube to the sphere
+        sphere.transform.position = cube.transform.position;
+        sphere.transform.rotation = cube.transform.rotation;
+    }
+
+}*/
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeCharacter : MonoBehaviour
 {
-    /*
-        /// <summary>
-        /// Change once after 10 seconds
-        /// </summary>
-        public GameObject cube; // Reference to the cube
-        public GameObject sphere; // Reference to the sphere
-        public float timeToChange = 5.0f; // Time in seconds to change the character
-
-        void Start()
-        {
-            // Ensure the sphere is inactive at the start
-            sphere.SetActive(false);
-            // Start the coroutine to change the character
-            StartCoroutine(ChangeCharacterFromCubeToSphere(timeToChange));
-
-
-            // Call ChangeCharacterFromSphereToCube after timeToChange seconds
-            StartCoroutine(DelayedSwitchToCube(timeToChange));
-        }
-
-        IEnumerator ChangeCharacterFromCubeToSphere(float time)
-        {
-            // Wait for the specified time
-            yield return new WaitForSeconds(time);
-
-            // Deactivate the cube and activate the sphere
-            cube.SetActive(false);
-            sphere.SetActive(true);
-
-            // Optional: transfer the position and rotation of the cube to the sphere
-            sphere.transform.position = cube.transform.position;
-            sphere.transform.rotation = cube.transform.rotation;
-        }
-
-        IEnumerator DelayedSwitchToCube(float time)
-        {
-            // Wait for timeToChange seconds
-            yield return new WaitForSeconds(time);
-
-            // Call the coroutine to switch back to cube
-            StartCoroutine(ChangeCharacterFromSphereToCube(timeToChange));
-        }
-
-        IEnumerator ChangeCharacterFromSphereToCube(float time)
-        {
-            // Wait for the specified time
-            yield return new WaitForSeconds(time);
-
-            // Deactivate the sphere and activate the cube
-            sphere.SetActive(false);
-            cube.SetActive(true);
-
-            // Optional: transfer the position and rotation of the sphere to the cube
-            cube.transform.position = sphere.transform.position;
-            cube.transform.rotation = sphere.transform.rotation;
-        }
-    */
-
-
-
-
-    /// <summary>
-    /// Change every 10 repeatedly
-    /// </summary>
     public GameObject cube; // Reference to the cube
     public GameObject sphere; // Reference to the sphere
-    public float switchInterval = 10.0f; // Time in seconds between switches
+    public GameObject octahedron; // Reference to the octahedron
+    public float timeToChange = 5.0f; // Time in seconds to change the character
 
     void Start()
     {
-        // Ensure the cube is active and sphere is inactive at the start
-        cube.SetActive(true);
+        // Ensure the sphere and octahedron are inactive at the start
         sphere.SetActive(false);
+        octahedron.SetActive(false);
 
-        // Start the coroutine to switch characters
-        StartCoroutine(SwitchCharacters());
+        // Start the coroutine to change the character
+        StartCoroutine(ChangeCharacterSequence());
     }
 
-    IEnumerator SwitchCharacters()
+    IEnumerator ChangeCharacterSequence()
     {
-        while (true)
-        {
-            // Wait for switchInterval seconds
-            yield return new WaitForSeconds(switchInterval);
+        // Change from cube to sphere
+        yield return ChangeCharacterFromTo(cube, sphere, timeToChange);
 
-            // Toggle between cube and sphere
-            if (cube.activeSelf)
-            {
-                // Switch from cube to sphere
-                cube.SetActive(false);
-                sphere.SetActive(true);
+        // Change from sphere to octahedron
+        yield return ChangeCharacterFromTo(sphere, octahedron, timeToChange);
+    }
 
-                // Optional: transfer the position and rotation of the cube to the sphere
-                sphere.transform.position = cube.transform.position;
-                sphere.transform.rotation = cube.transform.rotation;
-            }
-            else
-            {
-                // Switch from sphere to cube
-                sphere.SetActive(false);
-                cube.SetActive(true);
+    IEnumerator ChangeCharacterFromTo(GameObject from, GameObject to, float time)
+    {
+        // Wait for the specified time
+        yield return new WaitForSeconds(time);
 
-                // Optional: transfer the position and rotation of the sphere to the cube
-                cube.transform.position = sphere.transform.position;
-                cube.transform.rotation = sphere.transform.rotation;
-            }
-        }
+        // Deactivate the current object and activate the new object
+        from.SetActive(false);
+        to.SetActive(true);
+
+        // Optional: transfer the position and rotation of the current object to the new object
+        to.transform.position = from.transform.position;
+        to.transform.rotation = from.transform.rotation;
     }
 }
